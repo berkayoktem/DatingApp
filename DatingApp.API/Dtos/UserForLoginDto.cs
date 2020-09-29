fea3c74236c99824b0885f2d1f5a3e0fd68f885c
ibmboktem <berkay.oktem@gmail.com>
2020-09-29 13:46:45 +0300

    test

--- DatingApp.API/Dtos/UserForLoginDto.cs
+++ DatingApp.API/Dtos/UserForLoginDto.cs
@@ -2,6 +2,7 @@ namespace DatingApp.API.Dtos
 {
     public class UserForLoginDto
     {
+        // git contribution test
         public string Username { get; set; }
         public string Password { get; set; }
     }
